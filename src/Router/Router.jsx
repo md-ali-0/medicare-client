ed87de7655be25025aeae8859f6c377837adbe4c
md-ali-0 <mohammad.98482@gmail.com>
2023-11-24 19:06:21 +0600

    MediCare Client Structure

---
+++ src/Router/Router.jsx
@@ -0,0 +1,17 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layout/Main/Main";
+import Home from "../Pages/Home/Home";
+const Router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main/>,
+        children: [
+            {
+                path: '/',
+                element: <Home/>
+            },
+        ]
+    }
+])
+
+export default Router;
